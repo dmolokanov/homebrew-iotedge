187275f84d46811c145793c5efdea61ae686ac65
dmolokanov <dmolokanov@users.noreply.github.com>
2019-06-28 22:58:16 -0700

    Make formula build and install iotedged and iotedge binaries

--- iotedge.rb
+++ iotedge.rb
@@ -1,39 +1,44 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Iotedge < Formula
-  desc ""
-  homepage ""
-  url "https://raw.githubusercontent.com/dmolokanov/iotedge-releases/master/iotedge-1.0.8-rc1.tar.gz"
-  sha256 "9e37afe1b7be306857c596a5e3ebc8c1e1d00bfcd9507443a79f023efeb0dafd"
-  # depends_on "git" => :build
+  desc "Azure IoT Edge is an Internet of Things (IoT) service that builds on top of IoT Hub."
+  homepage "https://docs.microsoft.com/en-us/azure/iot-edge/"
+  url "https://raw.githubusercontent.com/dmolokanov/iotedge-releases/master/iotedge-1.0.8-rc2.tar.gz"
+  sha256 "4caabc955b12d95123bf8df53eac7d473ac9f19f0fe0a63864aca8fcc0410287"
   depends_on "cmake" => :build
   depends_on "rust" => :build
   depends_on "openssl"
-
+  
   def install
-    # # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Ensure that the `openssl` crate picks up the intended library.
+    # ensure that the `openssl` crate picks up the intended library
     # https://crates.io/crates/openssl#manual-configuration
     ENV["OPENSSL_DIR"] = Formula["openssl"].opt_prefix
-    ENV["FORCE_NO_UNITTEST"] = "ON"
-    # ENV["LIBIOTHSM_NOBUILD"] = "ON" # it'll cause cargo linker to use whatever libs were built before
+    ENV["OPENSSL_ROOT_DIR"] = Formula["openssl"].opt_prefix
 
+    # prevent build and run unit tests for C code
+    ENV["FORCE_NO_UNITTEST"] = "ON"
+
+    # prevent update submodules for git archive 
+    ENV["FORCE_NO_SUBMODULES_UPDATE"] = "ON"
+  
+    # build edgelet
     Dir.chdir("edgelet")
     system "make", "release"
+
+    # move files to appropriate locations
+    bin.install "target/release/iotedged"
+    bin.install "target/release/iotedge"
+    lib.install Dir["target/release/build/hsm-sys*/out/lib/*.dylib"]
+    etc.install "contrib/config/linux/config.yaml" # TODO change to macos config
+    # TODO logrotate
+    # TODO prepare to run as a service
+    man1.install "contrib/man/man1/iotedge.1"
+    man8.install "contrib/man/man8/iotedged.8"
+    doc.install "contrib/docs/LICENSE"
+    doc.install "contrib/docs/ThirdPartyNotices"
+    doc.install "contrib/docs/trademark"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test iotedge`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "iotedged", "-V"
+    system "iotedge", "-V"
   end
 end
